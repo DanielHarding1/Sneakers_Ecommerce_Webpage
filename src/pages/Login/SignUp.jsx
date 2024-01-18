87b0a24bd22949f0ae9eedb655e79912e3ca10a9
Daniel Harding <danielharding2008@gmail.com>
2024-01-18 12:17:39 +0000

    CSS added

--- src/pages/Login/SignUp.jsx
+++ src/pages/Login/SignUp.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { supabase } from "./client";
 import { Link } from "react-router-dom";
+import "./styleSign.css";
 
 const SignUp = () => {
   const [formData, setFormData] = useState({
@@ -38,34 +39,59 @@ const SignUp = () => {
 
   return (
     <form id="signup" onSubmit={handleLogin}>
-      <h3>Sign Up</h3>{" "}
-      <div className="form-group">
-        <label htmlFor="exampleInputEmail1">Email address:</label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          onChange={handleChange}
-        ></input>
-        <small id="emailHelp" className="form-text text-muted"></small>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-6 col-md-7 intro-section">
+            <div className="brand-wrapper">
+              <h1>
+                <a href="https://stackfindover.com/">Logo</a>
+              </h1>
+            </div>
+            <div className="intro-content-wrapper">
+              <h1 className="intro-title"></h1>
+              <p className="intro-text"></p>
+            </div>
+          </div>
+          <div className="col-sm-6 col-md-5 form-section">
+            <div className="login-wrapper">
+              <h2 className="login-title">Sign Up</h2>
+
+              <div className="form-group">
+                <label htmlFor="email" className="sr-only"></label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="form-control"
+                  placeholder="Email"
+                  onChange={handleChange}
+                ></input>
+              </div>
+              <div className="form-group mb-3">
+                <label htmlFor="password" className="sr-only"></label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  className="form-control"
+                  placeholder="Password"
+                  onChange={handleChange}
+                ></input>
+              </div>
+              <div className="d-flex justify-content-between align-items-center mb-5">
+                <button>Login</button>
+                <a href="#!" className="forgot-password-link">
+                  Password?
+                </a>
+              </div>
+              <p className="login-wrapper-footer-text">
+                {" "}
+                Don't Have an Account? <Link to="/Login">Login</Link>{" "}
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-      <br></br>
-      <div className="form-group">
-        <label htmlFor="exampleInputPassword1">Password</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          placeholder="Password"
-          onChange={handleChange}
-        ></input>
-      </div>
-      <button type="submit" className="btn btn-primary">
-        Submit
-      </button>
-      <br></br>
-      <br></br>
-      Already have an Account? <Link to="/Login">Login</Link>
     </form>
   );
 };
