d0a56808b98fa763811953a7036bebb6ba9c9489
ahmadsaqib1 <ahmadsaqib1@gmail.com>
2024-01-19 12:30:55 +0500

    UI for homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,77 @@
 import React from "react";
+import { Row, Col, Card } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="mt-5 ml-2">
+      
+      <Row >
+        {/* First Row */}
+        <Col xs={12} md={4}>
+          <Card className="project-card">
+            <Card.Img variant="top" src="/path/to/image1.jpg" alt="img" />
+            <Card.Body>
+              <Card.Title></Card.Title>
+              <NavLink to="/project1"></NavLink>
+            </Card.Body>
+          </Card>
+        </Col>
+
+        <Col xs={12} md={4}>
+          <Card className="project-card">
+            <Card.Img variant="top" src="/path/to/image2.jpg" alt="img" />
+            <Card.Body>
+              <Card.Title></Card.Title>
+              <NavLink to="/project2"></NavLink>
+            </Card.Body>
+          </Card>
+        </Col>
+
+        <Col xs={12} md={4}>
+          <Card className="project-card">
+            <Card.Img variant="top" src="/path/to/image3.jpg" alt="img" />
+            <Card.Body>
+              <Card.Title></Card.Title>
+              <NavLink to="/project3"></NavLink>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+
+      <Row className="mt-3">
+        {/* Second Row */}
+        <Col xs={12} md={4}>
+          <Card className="project-card">
+            <Card.Img variant="top" src="/path/to/image4.jpg" alt="img" />
+            <Card.Body>
+              <Card.Title></Card.Title>
+              <NavLink to="/project4"></NavLink>
+            </Card.Body>
+          </Card>
+        </Col>
+
+        <Col xs={12} md={4}>
+          <Card className="project-card">
+            <Card.Img variant="top" src="/path/to/image5.jpg" alt="img" />
+            <Card.Body>
+              <Card.Title></Card.Title>
+              <NavLink to="/project5"></NavLink>
+            </Card.Body>
+          </Card>
+        </Col>
+
+        <Col xs={12} md={4}>
+          <Card className="project-card">
+            <Card.Img variant="top" src="/path/to/image6.jpg" alt="img" />
+            <Card.Body>
+              <Card.Title></Card.Title>
+              <NavLink to="/project6"></NavLink>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     </div>
   );
 };
--- src/pages/Nav/Navbar.jsx
+++ src/pages/Nav/Navbar.jsx
@@ -1,8 +1,45 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import LogoImg from "../../assets/logo.png"
+import Img1 from "../../assets/shoe1.jpeg"
+import Img2 from "../../assets/shoe2.jpeg"
+import Img3 from "../../assets/shoe3.jpeg"
+
+import "./Navbar.css"
+
 const Navbar = () => {
   return (
-    <div>
-      <h1></h1>
-    </div>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid p-3">
+        <div className="row">
+          <div className="col-md-3">
+          <a className="nav-link d-flex align-items-center" href="#">
+              <img src={LogoImg} alt="Logo" className="img-fluid" />
+            </a>
+          </div>
+          <div className="col-md-3 d-flex align-items-center">
+            <a className="nav-link name" href="#">Sneakers</a>
+          </div>
+          <div className="col-md-6 d-flex align-items-center">
+          <div id="carouselExample" className="carousel slide" data-bs-ride="carousel">
+              <div className="carousel-inner">
+                <div className="carousel-item active">
+                  <img src={Img1} className=" d-block w-100" alt="First slide" />
+                </div>
+                <div className="carousel-item">
+                  <img src={Img2} className="d-block w-100" alt="Second slide" />
+                </div>
+                <div className="carousel-item">
+                  <img src={Img3} className="d-block w-100" alt="Third slide" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+    </nav>
   );
 };
 
