de006751723182bfbe5f5e077ce4462c401f9b41
Daniel Harding <danielharding2008@gmail.com>
2024-01-23 19:24:54 +0000

    Full functionality workiing

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -1,48 +1,45 @@
-import React from 'react';
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import "./Header.css"
+import "./Header.css";
 
 const Header = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light mt-2">
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-md-8">
-        <ul className="nav-menu">
-            <li>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-8">
+            <ul className="nav-menu">
+              <li>
                 <Link to="/">Brand Developers</Link>
-            </li>
-            <li>
+              </li>
+              <li>
                 <Link to="/">Shoe Size</Link>
-            </li>
-            <li>
+              </li>
+              <li>
                 <Link to="/">Colour</Link>
-            </li>
-            <li>
+              </li>
+              <li>
                 <Link to="/">Price Range</Link>
-            </li>
-        </ul>
-        </div>
-        <div className="col-md-4">
-        <form className="d-flex">
+              </li>
+            </ul>
+          </div>
+          <div className="col-md-4">
+            <form className="d-flex">
               <input
-                className="form-control me-2"
+                className="seachbar form-control me-2"
                 type="search"
                 placeholder="Search"
                 aria-label="Search"
+                onChange={(e) => setSearch}
               />
-             
             </form>
+          </div>
         </div>
-       
       </div>
-      
-    </div>
-  </nav>
+    </nav>
   );
 };
 
