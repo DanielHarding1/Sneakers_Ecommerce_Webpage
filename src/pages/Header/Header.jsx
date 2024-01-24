c54dae6633294672514e23678a2d8e8187bbd212
ahmadsaqib1 <ahmadsaqib1@gmail.com>
2024-01-25 03:41:24 +0500

    format header code

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -1,46 +1,44 @@
-import React from 'react';
-import { Link } from "react-router-dom"
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import "./Header.css"
+import "./Header.css";
 
 const Header = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light mt-2">
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-md-8">
-        <ul className="nav-menu">
-            <li>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-8">
+            <ul className="nav-menu">
+              <li>
                 <Link to="/">Brand Developers</Link>
-            </li>
-            <li>
+              </li>
+              <li>
                 <Link to="/">Shoe Size</Link>
-            </li>
-            <li>
+              </li>
+              <li>
                 <Link to="/">Colour</Link>
-            </li>
-            <li>
+              </li>
+              <li>
                 <Link to="/">Price Range</Link>
-            </li>
-        </ul>
-        </div>
-        <div className="col-md-4">
-        <form className="d-flex">
+              </li>
+            </ul>
+          </div>
+          <div className="col-md-4">
+            <form className="d-flex">
               <input
                 className="form-control me-2"
                 type="search"
                 placeholder="Search"
                 aria-label="Search"
+                onChange={(e) => {}}
               />
-
             </form>
+          </div>
         </div>
-
       </div>
-
-    </div>
-  </nav>
+    </nav>
   );
 };
 
