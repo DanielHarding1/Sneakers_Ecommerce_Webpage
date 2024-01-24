6c2ab39a2616d29f4b537cd519b5e35b3db6f794
Daniel Harding <danielharding2008@gmail.com>
2024-01-24 20:37:14 +0000

    added api implimentation

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -1,11 +1,43 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import "./Header.css";
+import axios from "axios";
 
 const Header = () => {
+  const options = {
+    method: "GET",
+    url: "https://asos2.p.rapidapi.com/products/v2/list",
+    params: {
+      store: "US",
+      offset: "0",
+      categoryId: "12703",
+      limit: "6",
+      country: "US",
+      sort: "freshness",
+      currency: "USD",
+      sizeSchema: "US",
+      lang: "en-US",
+      q: "nike shoes",
+    },
+    headers: {
+      "X-RapidAPI-Key": "de2f7235a1msh51b406813c90eb3p167ae7jsnc2746392f895",
+      "X-RapidAPI-Host": "asos2.p.rapidapi.com",
+    },
+  };
+
+  async function dataCollection() {
+    try {
+      const response = await axios.request(options);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  dataCollection();
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light mt-2">
       <div className="container-fluid">
